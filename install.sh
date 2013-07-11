1362fdb933948fee3d0ab4e0c041cb96492ff81a
Andy Schott <schottac@gmail.com>
2013-07-11 18:30:36 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f ~/.bash_profile
+ln -s ./bash_profile ~/.bash_profile
+
+rm -f ~/.bashrc
+ln -s ./bashrc ~/.bashrc
+
+rm -f ~/.emacs
+ln -s ./emacs ~/.emacs
+
+rm -f ~/.gitconfig
+ln -s ./gitconfig ~/.gitconfig
+
+rm -f ~/.gitignore
+ln -s ./gitignore ~/.gitignore
